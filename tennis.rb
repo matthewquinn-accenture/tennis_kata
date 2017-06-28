a66a421add6244680a7c1a6c1614224c58736548
Milorad Felbapov <miloradfelbapov@Milorads-MacBook-Pro.local>
2017-06-28 12:02:07 -0400

    Repositioned methods

--- tennis.rb
+++ tennis.rb
@@ -15,10 +15,10 @@ class Game
   def score
     if check_winner?
       player_leading + " wins"
-    elsif isDeuce?
+    elsif is_deuce?
       "deuce"
     elsif check_advantage?
-       player_leading + " advantage"
+      player_leading + " advantage"
     elsif check_tie?
       player_tie
     else
@@ -28,32 +28,36 @@ class Game
 
   private
 
-  def isDeuce?
-    @player1 >= 3 && check_tie?
+  def is_deuce?
+    enough_for_deuce?(@player1) && check_tie?
   end
 
-  def check_advantage?
-    had_deuce? && (score_difference == 1)
-  end
-
-  def had_deuce?
-    @player1 >= 3 && @player2 >= 3
-  end
-
-  def player_leading
-    @player1 < @player2 ? "Player 2" : "Player 1"
-  end
-
-  def check_winner?
-    (scored_enough?(@player1) || scored_enough?(@player2)) && (score_difference >= 2)
-  end
-
-  def scored_enough?(player)
-    player > 3
+  def enough_for_deuce?(player)
+    player >= 3
   end
 
   def check_tie?
     @player1 == @player2
+  end
+
+  def check_advantage?
+    enough_for_deuce?(@player1) && enough_for_deuce?(@player2) && (score_difference == 1)
+  end
+
+  def score_difference
+    (@player1 - @player2).abs
+  end
+
+  def check_winner?
+    (enough_for_win?(@player1) || enough_for_win?(@player2)) && (score_difference >= 2)
+  end
+
+  def enough_for_win?(player)
+    player > 3
+  end
+
+  def player_leading
+    @player1 < @player2 ? "Player 2" : "Player 1"
   end
 
   def player_tie
@@ -62,9 +66,5 @@ class Game
 
   def player_score
     @possible_scores[@player1] + " - " + @possible_scores[@player2]
-  end
-
-  def score_difference
-     (@player1 - @player2).abs
   end
 end
